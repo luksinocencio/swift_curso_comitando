9982dec4604e59cfb6270d98bc19553602f64680
luksinocencio <luksinocencio@gmail.com>
2023-03-28 13:17:05 -0300

    removendo duplicidade de codigo usando o pattern de factory para criacao do sut

--- RestaurantDomain/RestaurantDomainTests/RestaurantDomainTests.swift
+++ RestaurantDomain/RestaurantDomainTests/RestaurantDomainTests.swift
@@ -3,9 +3,7 @@ import XCTest
 
 final class RestaurantDomainTests: XCTestCase {
     func test_initializer_remoteRestaurantLoader_and_validate_urlRequest() throws {
-        let anyURL = try XCTUnwrap(URL(string: "https://comitando.com.br"))
-        let client = NetworkClientSpy()
-        let sut = RemoteRestaurantLoader(url: anyURL, networkClient: client)
+        let (sut, client, anyURL) = makeSUT()
         
         sut.load() { _ in }
         
@@ -14,9 +12,7 @@ final class RestaurantDomainTests: XCTestCase {
     }
     
     func test_load_twice() throws {
-        let anyURL = try XCTUnwrap(URL(string: "https://comitando.com.br"))
-        let client = NetworkClientSpy()
-        let sut = RemoteRestaurantLoader(url: anyURL, networkClient: client)
+        let (sut, client, anyURL) = makeSUT()
         
         sut.load() { _ in }
         sut.load() { _ in }
@@ -25,9 +21,7 @@ final class RestaurantDomainTests: XCTestCase {
     }
     
     func test_load_and_returned_error_for_connectivity() throws {
-        let anyURL = try XCTUnwrap(URL(string: "https://comitando.com.br"))
-        let client = NetworkClientSpy()
-        let sut = RemoteRestaurantLoader(url: anyURL, networkClient: client)
+        let (sut, client, _) = makeSUT()
         
         let exp = expectation(description: "esperando retorno da closure")
         var returnResult: RemoteRestaurantLoader.Error?
@@ -45,9 +39,7 @@ final class RestaurantDomainTests: XCTestCase {
     }
     
     func test_load_and_returned_error_for_invalidData() throws {
-        let anyURL = try XCTUnwrap(URL(string: "https://comitando.com.br"))
-        let client = NetworkClientSpy()
-        let sut = RemoteRestaurantLoader(url: anyURL, networkClient: client)
+        let (sut, client, _) = makeSUT()
         
         let exp = expectation(description: "esperando retorno da closure")
         var returnResult: RemoteRestaurantLoader.Error?
@@ -62,6 +54,14 @@ final class RestaurantDomainTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
         
         XCTAssertEqual(returnResult, .invalidData)
+    }
+    
+    private func makeSUT()  -> (RemoteRestaurantLoader, client: NetworkClientSpy, anyURL: URL) {
+        let anyURL = URL(string: "https://comitando.com.br")!
+        let client = NetworkClientSpy()
+        let sut = RemoteRestaurantLoader(url: anyURL, networkClient: client)
+        
+        return (sut, client, anyURL)
     }
 }
 
