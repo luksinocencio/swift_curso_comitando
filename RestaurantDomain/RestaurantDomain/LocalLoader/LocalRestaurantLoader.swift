232d68f6775ffbd8db4c4860d95a31778715f393
luksinocencio <luksinocencio@gmail.com>
2023-04-16 11:39:56 -0300

    implementando camada LocalRestaurantLoader com eventos deletar e salvar novos dados em cache

--- RestaurantDomain/RestaurantDomain/LocalLoader/LocalRestaurantLoader.swift
+++ RestaurantDomain/RestaurantDomain/LocalLoader/LocalRestaurantLoader.swift
@@ -1,1 +1,26 @@
 import Foundation
+
+protocol CacheClient {
+    func save(_ items: [RestaurantItem], timestamp: Date, completion: @escaping(Error?) -> Void)
+    func delete(completion: @escaping (Error?) -> Void)
+}
+
+final class LocalRestaurantLoader {
+    let cache: CacheClient
+    let currentDate: () -> Date
+    
+    init(cache: CacheClient, currentDate: @escaping () -> Date) {
+        self.cache = cache
+        self.currentDate = currentDate
+    }
+    
+    func save(_ items: [RestaurantItem], completion: @escaping (Error?) -> Void) {
+        cache.delete { [unowned self] error in
+            if error == nil {
+                self.cache.save(items, timestamp: self.currentDate(), completion: completion)
+            } else {
+                completion(error)
+            }
+        }
+    }
+}
