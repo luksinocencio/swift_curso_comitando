7c605809bb764234ab670ea1b172652de575a88b
luksinocencio <luksinocencio@gmail.com>
2023-06-22 22:29:47 -0300

    criacao do modulo RestarauntUI e testes iniciais

--- RestaurantDomain/RestaurantUI/RestaurantListViewController.swift
+++ RestaurantDomain/RestaurantUI/RestaurantListViewController.swift
@@ -2,13 +2,14 @@ import UIKit
 import RestaurantDomain
 
 final class RestaurantListViewController: UIViewController {
-    
     private(set) var restaurantCollection: [RestaurantItem] = []
     private var service: RestaurantLoader? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        service?.load(completion: { result in
+        
+        service?.load(completion: { [weak self] result in
+            guard let self = self else { return }
             switch result {
                 case let .success(items):
                     self.restaurantCollection = items
--- RestaurantDomain/RestaurantUITests/RestaurantUITests.swift
+++ RestaurantDomain/RestaurantUITests/RestaurantUITests.swift
@@ -1,33 +1,57 @@
 import XCTest
 import RestaurantDomain
-@testable import RestaurantUI
 
+@testable import RestaurantUI
 
 final class RestaurantUITests: XCTestCase {
     func test_init_does_not_load() {
-        let service = RestaurantLoaderSpy()
-        let sut = RestaurantListViewController(service: service)
+        let (_, service) = makeSUT()
         
-//        XCTAssertEqual(sut.restaurantCollection.count, 0)
         XCTAssertEqual(service.loadCount, 0)
     }
     
     func test_viewDidLoad_should_be_called_load_service() {
-        let service = RestaurantLoaderSpy()
-        let sut = RestaurantListViewController(service: service)
+        let (sut, service) = makeSUT()
         
-        // chama a nossa view com todo o ciclo de vida
         sut.loadViewIfNeeded()
         
         XCTAssertEqual(service.loadCount, 1)
+    }
+    
+    func test_load_returned_restaurantItems_data_and_restaurantCollection_does_not_empty() {
+        let (sut, service) = makeSUT()
+        
+        sut.loadViewIfNeeded()
+        service.completionSuccess(.success([makeItem()]))
+        
+        XCTAssertEqual(service.loadCount, 1)
+        XCTAssertEqual(sut.restaurantCollection.count, 1)
+    }
+}
+
+extension RestaurantUITests {
+    private func makeSUT(
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> (sut: RestaurantListViewController, service: RestaurantLoaderSpy) {
+        let service = RestaurantLoaderSpy()
+        let sut = RestaurantListViewController(service: service)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(service, file: file, line: line)
+        return (sut, service)
     }
 }
 
 final class RestaurantLoaderSpy: RestaurantLoader {
     private(set) var loadCount = 0
+    private var completionLoadHandler: ((RestaurantResult) -> Void)?
     
-    func load(completion: @escaping (Result<[RestaurantDomain.RestaurantItem], RestaurantDomain.RestaurantResultError>) -> Void) {
+    func load(completion: @escaping (RestaurantResult) -> Void) {
         loadCount += 1
+        completionLoadHandler = completion
+    }
+    
+    func completionSuccess(_ result: RestaurantResult) {
+        completionLoadHandler?(result)
     }
 }
-
