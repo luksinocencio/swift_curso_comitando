441b1797010df2add6b126197b0f248187a5bf87
luksinocencio <luksinocencio@gmail.com>
2023-06-26 23:25:55 -0300

    criando padrão Observer para simplificação da camada de ViewModel

--- RestaurantDomain/RestaurantUI/RestaurantListViewModel.swift
+++ RestaurantDomain/RestaurantUI/RestaurantListViewModel.swift
@@ -2,14 +2,16 @@ import Foundation
 import RestaurantDomain
 
 final class RestaurantListViewModel {
+    typealias Observer<T> = (T) -> Void
+    
     private let service: RestaurantLoader
     
     init(service: RestaurantLoader) {
         self.service = service
     }
     
-    var onLoadingState: ((Bool) -> Void)?
-    var onRestaurantItem: (([RestaurantItem]) -> Void)?
+    var onLoadingState: Observer<Bool>?
+    var onRestaurantItem: Observer<[RestaurantItem]>?
     
     func loadService() {
         onLoadingState?(true)
