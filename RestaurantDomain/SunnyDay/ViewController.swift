aa0035a18181b9ea0450d8b39ad534b276c7f605
luksinocencio <luksinocencio@gmail.com>
2023-07-02 17:10:12 -0300

    criando estrutura de projeto

---
+++ RestaurantDomain/SunnyDay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SunnyDay
+//
+//  Created by Lucas Inocencio on 02/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
