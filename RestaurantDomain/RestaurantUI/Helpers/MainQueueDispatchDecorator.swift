b8f817a9a8068924d89625fa6594ecf77c3c50af
luksinocencio <luksinocencio@gmail.com>
2023-07-02 17:01:08 -0300

    criando helpers para MainQueueDispatchDecorator

---
+++ RestaurantDomain/RestaurantUI/Helpers/MainQueueDispatchDecorator.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+final class MainQueueDispatchDecorator<T> {
+    let decoratee: T
+    
+    init(decoratee: T) {
+        self.decoratee = decoratee
+    }
+    
+    func dispatch(completion: @escaping () -> Void) {
+        guard Thread.isMainThread else {
+            return DispatchQueue.main.async(execute: completion)
+        }
+        completion()
+    }
+}
+
+//extension DispatchQueue {
+//    func safeAsync(_ block: @escaping () -> Void) {
+//        if self === DispatchQueue.main && Thread.isMainThread {
+//            block()
+//        } else {
+//            async { block() }
+//        }
+//    }
+//}
--- RestaurantDomain/RestaurantUI/RestaurantListCompose.swift
+++ RestaurantDomain/RestaurantUI/RestaurantListCompose.swift
@@ -14,41 +14,6 @@ final class RestaurantListCompose {
     }
 }
 
-
-final class MainQueueDispatchDecorator<T> {
-    private let decoratee: T
-    
-    init(decoratee: T) {
-        self.decoratee = decoratee
-    }
-    
-    func dispatch(completion: @escaping () -> Void) {
-        guard Thread.isMainThread else {
-            return DispatchQueue.main.async(execute: completion)
-        }
-        
-        completion()
-    }
-    
-//    func load(completion: @escaping (Result<[RestaurantDomain.RestaurantItem], RestaurantDomain.RestaurantResultError>) -> Void) {
-//        decoratee.load { result in
-//            DispatchQueue.main.safeAsync {
-//                completion(result)
-//            }
-//        }
-//    }
-}
-
-//extension DispatchQueue {
-//    func safeAsync(_ block: @escaping () -> Void) {
-//        if self === DispatchQueue.main && Thread.isMainThread {
-//            block()
-//        } else {
-//            async { block() }
-//        }
-//    }
-//}
-
 extension MainQueueDispatchDecorator: RestaurantLoader where T == RestaurantLoader {
     func load(completion: @escaping (Result<[RestaurantDomain.RestaurantItem], RestaurantDomain.RestaurantResultError>) -> Void) {
         decoratee.load { [weak self] result in
