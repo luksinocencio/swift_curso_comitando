57e16148e7f35ce705a8dfe1adc458a61b557e65
luksinocencio <luksinocencio@gmail.com>
2023-07-02 13:40:56 -0300

    ajustando nome da interface para RestaurantListViewController e criando teste de snapshot para nossa UI

--- RestaurantDomain/RestaurantUI/RestaurantListCompose.swift
+++ RestaurantDomain/RestaurantUI/RestaurantListCompose.swift
@@ -7,6 +7,7 @@ final class RestaurantListCompose {
         let interactor = RestaurantListInteractor(service: service, presenter: presenter)
         let controller = RestaurantListViewController(interactor: interactor)
         presenter.view = controller
+        controller.title = "Praia do Forte"
         
         return controller
     }
--- RestaurantDomain/RestaurantUITests/RestaurantUISnaphotTests.swift
+++ RestaurantDomain/RestaurantUITests/RestaurantUISnaphotTests.swift
@@ -12,12 +12,26 @@ final class RestaurantUISnaphotTests: XCTestCase {
         RestaurantItem(id: UUID(), name: "Tenda do quartel", location: "Canto do Forte - Praia Grande", distance: 50, ratings: 4, parasols: 10)
     ]
     
+    /// Rodar os tetes com device iPhone 14 Pro
     func test_snapshot_after_render_restaurantItemCell() {
         let controller = RestaurantItemCellController(viewModel: dataModel[0])
         let cell = RestaurantItemCell(style: .default, reuseIdentifier: RestaurantItemCell.identifier)
+        
         cell.backgroundColor = .white
         controller.renderCell(cell)
-        assertSnapshot(matching: cell, as: .image(size: CGSize(width: 375, height: 175)))
+        
+        assertSnapshot(matching: cell, as: .image(size: CGSize(width: 375, height: 175)), record: false)
+    }
+    
+    /// Rodar os tetes com device iPhone 14 Pro
+    func test_snapshot_render_restaurantListViewController() {
+        let (sut, service) = makeSUT()
+        let navigation = UINavigationController(rootViewController: sut)
+        
+        sut.loadViewIfNeeded()
+        service.completionResult(.success(dataModel))
+     
+        assertSnapshot(matching: navigation, as: .image(on: .iPhoneX(.portrait)), record: false)
     }
 }
 
