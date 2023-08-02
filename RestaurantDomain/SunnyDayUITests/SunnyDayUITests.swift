8b4a32d87924dc7e2c1cd380dfe2679274ef3be2
luksinocencio <luksinocencio@gmail.com>
2023-08-01 22:29:46 -0300

    criando primeiro teste de UI Validando numero de cells e textos estaticos de um usuario conectado

--- RestaurantDomain/SunnyDayUITests/SunnyDayUITests.swift
+++ RestaurantDomain/SunnyDayUITests/SunnyDayUITests.swift
@@ -1,41 +1,16 @@
-//
-//  SunnyDayUITests.swift
-//  SunnyDayUITests
-//
-//  Created by Lucas Inocencio on 02/07/23.
-//
-
 import XCTest
 
 final class SunnyDayUITests: XCTestCase {
-
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    
+    func test_launch_should_be_show_remote_data_with_client_have_connectivity() {
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+        
+        XCTAssertEqual(app.cells.count, 10)
+        XCTAssertEqual(app.cells.firstMatch.staticTexts.count, 4)
     }
 }
