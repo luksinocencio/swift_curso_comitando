44b914ac2e782927e3a9726354cc773f7985582a
luksinocencio <luksinocencio@gmail.com>
2023-08-01 23:09:16 -0300

    criando cenário de cliente offline e fazendo o uso do CommandLine.arguments e UserDefaults.standard

--- RestaurantDomain/SunnyDay/SceneDelegate.swift
+++ RestaurantDomain/SunnyDay/SceneDelegate.swift
@@ -6,31 +6,26 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
     
+    private lazy var fileManagerURL = {
+        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appending(path: "SunnyDay.store")
+    }()
+    
     private lazy var localService = {
-        let fileManagerURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appending(path: "SunnyDay.store")
+        let fileManagerURL = fileManagerURL
         let cache = CacheService(manegerURL: fileManagerURL)
         
         return LocalRestaurantLoader(cache: cache, currentDate: Date.init)
     }()
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
         
-        // RemoteService
-//        let session = URLSession(configuration: .ephemeral)
-//        let network = NetworkService(session: session)
-//        let url = URL(string: "https://raw.githubusercontent.com/comitando/assets/main/api/restaurant_list_endpoint.json")!
-//        let remoteService = RemoteRestaurantLoader(url: url, networkClient: network)
-        
-//        // LocalService
-//        let fileManagerURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appending(path: "SunnyDay.store")
-//        let cache = CacheService(manegerURL: fileManagerURL)
-//        let localService = LocalRestaurantLoader(cache: cache) {
-//            return Date()
-//        }
-        
+        if CommandLine.arguments.contains("-reset") {
+            try? FileManager.default.removeItem(at: fileManagerURL)
+        }
+                
         // composite remote and local
-        let compositeService = RestaurantLoaderCompositeRemoteAndLocal(main: remoteService(), fallback: localService)
+        let compositeService = RestaurantLoaderCompositeRemoteAndLocal(main: makeRemoteLoader(), fallback: localService)
         
         // Decorator side effects Service and insert cache
         let decoratorService = RestaurantLoaderCacheDecorator(decoratee: compositeService, cache: localService)
@@ -44,36 +39,44 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.makeKeyAndVisible()
     }
     
-    private func remoteService() -> RemoteRestaurantLoader {
+    private func remoteService() -> RestaurantLoader {
         let session = URLSession(configuration: .ephemeral)
         let network = NetworkService(session: session)
         let url = URL(string: "https://raw.githubusercontent.com/comitando/assets/main/api/restaurant_list_endpoint.json")!
         
         return RemoteRestaurantLoader(url: url, networkClient: network)
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-        
+    
+    private func makeRemoteLoader() -> RestaurantLoader {
+        switch UserDefaults.standard.string(forKey: "connectivity") {
+            case "offline":
+                return RestaurantLoaderMock()
+            default:
+                return remoteService()
+        }
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
         localService.validateCache()
     }
+}
 
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
+private final class RestaurantLoaderMock: RestaurantLoader {
+    func load(completion: @escaping (Result<[RestaurantDomain.RestaurantItem], RestaurantDomain.RestaurantResultError>) -> Void) {
+        completion(.failure(.connectivity))
     }
 }
 
+
+// RemoteService
+//        let session = URLSession(configuration: .ephemeral)
+//        let network = NetworkService(session: session)
+//        let url = URL(string: "https://raw.githubusercontent.com/comitando/assets/main/api/restaurant_list_endpoint.json")!
+//        let remoteService = RemoteRestaurantLoader(url: url, networkClient: network)
+
+//        // LocalService
+//        let fileManagerURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appending(path: "SunnyDay.store")
+//        let cache = CacheService(manegerURL: fileManagerURL)
+//        let localService = LocalRestaurantLoader(cache: cache) {
+//            return Date()
+//        }
--- RestaurantDomain/SunnyDayUITests/SunnyDayUITests.swift
+++ RestaurantDomain/SunnyDayUITests/SunnyDayUITests.swift
@@ -13,4 +13,25 @@ final class SunnyDayUITests: XCTestCase {
         XCTAssertEqual(app.cells.count, 10)
         XCTAssertEqual(app.cells.firstMatch.staticTexts.count, 4)
     }
+    
+    func test_launch_should_be_show_cache_data_when_client_does_have_connectivity() {
+        let online = XCUIApplication()
+        online.launchArguments = ["-reset"]
+        online.launch()
+        
+        let offline = XCUIApplication()
+        offline.launchArguments = ["-connectivity", "offline"]
+        offline.launch()
+        
+        XCTAssertEqual(offline.cells.count, 10)
+        XCTAssertEqual(offline.cells.firstMatch.staticTexts.count, 4)
+    }
+    
+    func test_launch_should_be_show_empty_data_when_client_does_not_have_connectivity_and_cache() {
+        let app = XCUIApplication()
+        app.launchArguments = ["-reset", "-connectivity", "offline"]
+        app.launch()
+        
+        XCTAssertEqual(app.cells.count, 0)
+    }
 }
